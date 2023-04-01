95f865dd8d308f4688304b10e693ff9bf3c1eccf
John Deadman <xSomoy@protonmail.com>
2023-04-01 17:18:07 +0600

    ajax test

--- script.js
+++ script.js
@@ -2,5 +2,9 @@ function loadData(){
     // Create New Request 
     const xhr = new XMLHttpRequest();
 
-    //  
+    // What to do when response arraives
+    xhr.onload = function (){
+        const container = document.getElementById('demo');
+        demo.innerHTML
+    } 
 }
