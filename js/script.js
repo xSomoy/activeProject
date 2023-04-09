a9f4299d49b1babc3fe3166a2a924ddddf5b530a
John Deadman <xSomoy@protonmail.com>
2023-04-09 14:02:12 +0600

    document formated

--- js/script.js
+++ js/script.js
@@ -1,4 +1,31 @@
 // S - Conmments 
+// S - Timer 
+function updateTimer() {
+    future = Date.parse("April 10, 2023 00:00:00");
+    now = new Date();
+    diff = future - now;
+
+    days = Math.floor(diff / (1000 * 60 * 60 * 24));
+    hours = Math.floor(diff / (1000 * 60 * 60));
+    mins = Math.floor(diff / (1000 * 60));
+    secs = Math.floor(diff / 1000);
+
+    d = days;
+    h = hours - days * 24;
+    m = mins - hours * 60;
+    s = secs - mins * 60;
+
+    document.getElementById("timer")
+        .innerHTML =
+        '<div>' + d + '<span>days</span></div>' +
+        '<div>' + h + '<span>hours</span></div>' +
+        '<div>' + m + '<span>minutes</span></div>' +
+        '<div>' + s + '<span>seconds</span></div>';
+}
+setInterval('updateTimer()', 1000);
+
+//   E-Timer 
+
 $(function () {
 
     var owl = $('.slide-one-item');
